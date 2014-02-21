94d379a81e77437c072403eab482bab9e43cb407
Tom Shaw <tom@urlrider.com>
2014-02-21 08:52:42 -0600

    first commit

--- config.rb
+++ config.rb
@@ -1,7 +1,20 @@
+# Compass documentation.
+# http://sass-lang.com/documentation/file.SASS_REFERENCE.html
+
+# New setup.
 require 'bootstrap-sass'
-css_dir = 'public/css'
-sass_dir = 'public/sass'
-javascripts_dir = 'public/js'
+http_path = '/'
+project_path = 'public'
+css_dir = 'css'
+sass_dir = 'sass'
+javascripts_dir = 'js'
 line_comments = false
-output_style = :compressed
-# http://sass-lang.com/documentation/file.SASS_REFERENCE.html#output_style
+output_style = :expanded
+
+# Original setup.
+# require 'bootstrap-sass'
+# css_dir = 'public/css'
+# sass_dir = 'public/sass'
+# javascripts_dir = 'public/js'
+# line_comments = false
+# output_style = :compressed
--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,13 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.initConfig({
-    uglify: {
-      my_target: {
-        files: {
-          'public/js/script.js': ['public/js/app/script.js']
-        }
-      }
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner: '/*! <%=pkg.name%> - v<%=pkg.version%> (build <%=pkg.build%>) - ' + '<%=grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT")%> */'
     },
     compass: {
       dev: {
@@ -17,11 +16,38 @@ module.exports = function(grunt) {
         } 
       } 
     },
+    jshint: {
+      all: ['public/js/app/*.js']
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%=pkg.name%> - v<%=pkg.version%> (build <%=pkg.build%>) */',
+        mangle: false,
+        beautify: true,
+        preserveComments: true
+      },
+      my_target: {
+        files: {
+          'public/js/script.min.js': ['public/js/script.js']
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: [
+          'public/js/vendor/jquery/dist/jquery.min.js',
+          'public/js/vendor/underscore/underscore.js',
+          'public/js/vendor/bootstrap-sass/dist/js/bootstrap.min.js',
+          'public/js/app/script.js'
+        ],
+        dest: 'public/js/script.js'
+      }
+    },
     watch: {
       options: { livereload: true },
       scripts: {
         files: ['public/js/app/*.js'],
-        tasks: ['uglify']
+        tasks: ['concat','uglify']
       },
       sass: {
         files: ['public/sass/*.scss'],
@@ -36,4 +62,5 @@ module.exports = function(grunt) {
     } 
   })
   grunt.registerTask('default', ['watch']);
+  grunt.registerTask('js', ['concat','uglify']);
 } 
